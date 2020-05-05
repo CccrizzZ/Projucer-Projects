2899aa45ce968d1623b1d9b19a364d1640a32996
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2020-05-05 00:24:56 -0400

    ##

--- NewSynth/Source/MainComponent.cpp
+++ NewSynth/Source/MainComponent.cpp
@@ -28,16 +28,31 @@ MainComponent::MainComponent()
         setAudioChannels (2, 2);
     }
 
-    volume.setSliderStyle(Slider::LinearBar);
-    volume.setRange(Range<double>(0.0, 1.0), 0.1);
-    volume.setColour(Slider::ColourIds::trackColourId, Colours::limegreen);
-    volume.setColour(Slider::ColourIds::textBoxOutlineColourId, Colours::limegreen);
+    VolumeSlider.setSliderStyle(Slider::LinearBar);
+    VolumeSlider.setRange(Range<double>(0.0, 1.0), 0.01);
+    VolumeSlider.setColour(Slider::ColourIds::trackColourId, Colours::limegreen);
+    VolumeSlider.setColour(Slider::ColourIds::textBoxOutlineColourId, Colours::limegreen);
 
-    addAndMakeVisible(volume);
-    volumeLable.setText("Volume: ", dontSendNotification);
-    volumeLable.attachToComponent(&volume, true);
-    addAndMakeVisible(volumeLable);
+    addAndMakeVisible(VolumeSlider);
 
+
+    volumeLabel.setText("Volume: ", dontSendNotification);
+    volumeLabel.attachToComponent(&VolumeSlider, true);
+    addAndMakeVisible(volumeLabel);
+
+
+    FrequencySlider.setSliderStyle(Slider::LinearBar);
+    FrequencySlider.setRange(Range<double>(50.0, 5000.0), 1.0);
+    FrequencySlider.setSkewFactorFromMidPoint(500.0);
+    FrequencySlider.setTextValueSuffix("Hz");
+    FrequencySlider.setColour(Slider::ColourIds::trackColourId, Colours::limegreen);
+    FrequencySlider.setColour(Slider::ColourIds::textBoxOutlineColourId, Colours::limegreen);
+    addAndMakeVisible(FrequencySlider);
+
+
+    FrequencyLabel.setText("Frequency: ", dontSendNotification);
+    FrequencyLabel.attachToComponent(&FrequencySlider, true);
+    addAndMakeVisible(FrequencyLabel);
 
 }
 
@@ -59,20 +74,17 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
     // For more details, see the help for AudioProcessor::prepareToPlay()
 
 
-
-
-
-
-
-
-
-
     String message;
     message << "Preparing to play audio..." << newLine;
     message << " samplesPerBlockExpected = " << samplesPerBlockExpected << newLine;
     message << " sampleRate = " << sampleRate;
     Logger::getCurrentLogger()->writeToLog(message);
 
+
+
+
+    CurrentSampleRate = sampleRate;
+    updateAngleData();
 
 
 
@@ -86,9 +98,9 @@ void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFil
 
     // Right now we are not producing any data, in which case we need to clear the buffer
     // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    //bufferToFill.clearActiveBufferRegion();
 
-    auto level = (float)volume.getValue();
+    auto level = (float)VolumeSlider.getValue();
     auto LevelScale = level * 2.0f;
 
     for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
@@ -101,6 +113,18 @@ void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFil
         }
     }
 
+    auto FreqLevel = (float)FrequencySlider.getValue();
+    auto* LeftBuffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+    auto* RightBuffer = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
+
+    for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+    {
+        auto CurrentSample = (float)std::sin(CurrentAngle);
+        CurrentAngle += AngleDelta;
+        LeftBuffer[sample] = CurrentSample * level;
+        RightBuffer[sample] = CurrentSample * level;
+
+    }
 
 }
 
@@ -126,6 +150,32 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    volume.setBounds(100, 40, 150, 20);
+    VolumeSlider.setBounds(100, 40, 150, 20);
+
+    VolumeSlider.onValueChange = [this]
+    {
+        level = (float)VolumeSlider.getValue();
+    };
+
+
+
+    FrequencySlider.setBounds(100, 120, 150, 20);
+
+    FrequencySlider.onValueChange = [this]
+    {
+        if (CurrentSampleRate)
+        {
+            updateAngleData();
+        }
+    };
+        
+    
+
+}
+
+void MainComponent::updateAngleData()
+{
+    auto CyclesPerSample = FrequencySlider.getValue() / CurrentSampleRate;
+    AngleDelta = CyclesPerSample * 2.0 * MathConstants<double>::pi;
 
 }
--- NewSynth/Source/MainComponent.h
+++ NewSynth/Source/MainComponent.h
@@ -35,11 +35,25 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
+    
+    
+    // Volume level
+    float level = -18.0f;
+    // Volume slider and label
+    Slider VolumeSlider;
+    Label volumeLabel;
 
-    Slider volume;
-    Label volumeLable;
+    // random shit
     JUCE_API::Random random;
 
+    Slider FrequencySlider;
+    Label FrequencyLabel;
 
+    double CurrentSampleRate = 0.0;
+    double CurrentAngle = 0.0;
+    double AngleDelta = 0.0;
+
+
+    void updateAngleData();
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
